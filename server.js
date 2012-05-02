243130bee910a7300998becd8003fef490fe9ad8
Peter Cottle <petermcottle@gmail.com>
2012-05-02 15:17:26 -0700

    merging node server stuff

--- server.js
+++ server.js
@@ -1,46 +1,88 @@
-var fs = require('fs');
-
+﻿var fs = require('fs');
 var server = require('http').createServer(function(req, response){
-
-    var pageToGive = '/index.html';
-
-    fs.readFile(__dirname+pageToGive, function(err, data){
-
-          response.writeHead(200, {'Content-Type':'text/html'}); 
-          response.write(data);  
-          response.end();
-    });
+  fs.readFile(__dirname+'/multiroomchat.html', function(err, data){
+    response.writeHead(200, {'Content-Type':'text/html'}); 
+    response.write(data);  
+    response.end();
+  });
 });
 
-var port = process.env.PORT || 8080;
-server.listen(port);
-
-console.log("running server on ",port);
+server.listen(8080);
+console.log("server listening on port 8080");
 
 var nowjs = require("now");
 var everyone = nowjs.initialize(server, {socketio: {transports: ['xhr-polling', 'jsonp-polling']}});
 
+
 nowjs.on('connect', function(){
-    this.now.room = "lobby";
-    nowjs.getGroup(this.now.room).addUser(this.user.clientId);
-    console.log(this.now.name + " joined the lobby");
+  this.now.room = "room 1";
+  nowjs.getGroup(this.now.room).addUser(this.user.clientId);
+  console.log("Joined: " + this.user.clientId);
 });
+
 
 nowjs.on('disconnect', function(){
-    console.log("Left: " + this.now.name + "from room", this.now.room);
+  console.log("Someone left with client id",this.user.clientId);
 });
 
+
+roomInfo = {};
+
+everyone.now.makeRoom = function(newRoom,equationInfo) {
+
+    if(roomInfo[newRoom])
+    {
+        this.now.receiveMessage("That room is already created!");
+        return;
+    }
+
+    //we need to initialize some things about roomInnfo
+    roomInfo[newRoom] = equationInfo;
+
+    this.now.receiveMessage("You have now made the room " + newRoom + ", transferring you...");
+    this.now.changeRoom(newRoom);
+};
+
 everyone.now.changeRoom = function(newRoom){
-    nowjs.getGroup(this.now.room).removeUser(this.user.clientId);
-    nowjs.getGroup(newRoom).addUser(this.user.clientId);
 
-    this.now.room = newRoom;
+  if(!roomInfo[newRoom])
+  {
+      this.now.receiveMessage("You need to make a room first silly!");
+      return;
+  }
 
-    this.now.receiveMessage("SERVER", "You're now in " + this.now.room);
-    console.log(this.now.name, "joined the room ", this.now.room);
+  //tell only this client that they are leaving
+  this.now.distributeMessage("Hey everyone in group " + this.now.room + ", this person " + this.user.clientId + " is leaving");
+
+  //remove them from the group and get the new one
+  nowjs.getGroup(this.now.room).removeUser(this.user.clientId);
+  nowjs.getGroup(newRoom).addUser(this.user.clientId);
+
+  //set the room and tell them that they have joined
+  this.now.room = newRoom;
+  this.now.distributeMessage("Hey everyone in group " + this.now.room + ", this person" + this.user.clientId + " is entering!");
+
+  //we need to tell them how many are in the room
+  var _this = this;
+  nowjs.getGroup(this.now.room).count(function(count){
+
+        _this.now.receiveMessage("You're now in " + _this.now.room + " and worker number " + count);
+        _this.now.position = count;
+        _this.now.receiveEquation(roomInfo[_this.now.room]);
+        _this.now.distributeNewTotal(count);
+  });
 }
 
 everyone.now.distributeMessage = function(message){
-    nowjs.getGroup(this.now.room).now.receiveMessage(this.now.name, message);
+  nowjs.getGroup(this.now.room).now.receiveMessage(message);
 };
+
+everyone.now.distributeEquation = function(equationInfo) {
+    nowjs.getGroup(this.now.room).now.receiveEquation(equationInfo);
+};
+
+everyone.now.distributeNewTotal = function(totalCount) {
+    nowjs.getGroup(this.now.room).now.receiveTotal(totalCount);
+};
+
 
